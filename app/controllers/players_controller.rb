16394f6cfcb0eb8c63eadf71a92950d81c4ea009
Chazz Gagliano <cgags17285@gmail.com>
2023-04-02 21:59:47 -0400

    create action added

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,22 @@ class PlayersController < ApplicationController
     @players = Player.all
     render :index
   end
+  def show
+    @player = Player.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @player = Player.new(
+    name: params[:name],
+    jersey: params[:jersey],
+    team: params[:team],
+    position: params[:position]
+    )
+   @player.save
+   render :show
+  end
+
+  
+  
 end
